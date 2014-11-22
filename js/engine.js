e41f30d0202853c24ca91374df9fb7d9eed993cf
Cornelia <cornelia_schulz@hotmail.com>
2014-11-22 19:40:49 +1300

    fixed comments

--- js/engine.js
+++ js/engine.js
@@ -100,8 +100,6 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-
-        player.update();
     }
 
     /* This function initially draws the "game level", it will then call
